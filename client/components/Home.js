a3258e2f5d538fe07f6acf0bb36892261fbc53dd
Matthieu Brouillard <mbrouilla@gmail.com>
2019-03-04 09:41:00 +0100

    Change ajax call emplacement

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { MapView } from "expo";
+import axios from "axios";
 
 import Header from "./Header";
 
@@ -18,36 +19,39 @@ export default class Home extends Component {
         latitude: 0,
         longitude: 0
       },
+      itemsMarkers: [
+      ],
       loading: true
     };
   }
 
-  getUserLocation = () => {
-    navigator.geolocation.getCurrentPosition(position => {
+  getUserLocation = async () => {
+    await navigator.geolocation.getCurrentPosition(async position => {
       // First we get user location
-      this.setState(
-        {
-          userPosition: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          },
-          region: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-          }
+      await this.setState({
+        userPosition: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
         },
-        () => {
-          this.setState({
-            loading: false
-          });
+        region: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
         }
-      );
+      });
     });
+    
+    await axios
+      .get(`localhost:8080/api/items/`)
+      .then(items => {
+        this.setState({ itemsMarker: items });
+      })
+      .catch(err => console.log(err));
+    await this.setState({ loading: false });
   };
 
-  componentWillMount() {
+ componentDidMount() {
     this.getUserLocation();
   }
   render() {
@@ -64,6 +68,7 @@ export default class Home extends Component {
       return (
         <View style={styles.container}>
           <Header />
+          <Text></Text>
           <MapView style={{ flex: 1 }} region={this.state.userPosition} />
         </View>
       );
